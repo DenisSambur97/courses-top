37f4dbdd1ec77c449f85f26015d1f82eb3e6d75c
Denis <denissand1997@gmail.com>
2023-01-16 18:38:20 +0300

    Fixing

--- next.config.js
+++ next.config.js
@@ -1,31 +1,21 @@
-// module.exports = {
-//   images: {
-//     domains: ['courses-top.ru']
-//   },
-//   webpack(config, options) {
-//     config.module.rules.push({
-//       loader: '@svgr/webpack',
-//       options: {
-//         prettier: false,
-//         svgo: true,
-//         svgoConfig: {
-//           plugins: [{ removeViewBox: false }],
-//         },
-//         titleProp: true,
-//       },
-//       test: /\.svg$/,
-//     });
-//
-//     return config;
-//   },
-// };
 module.exports = {
-  webpack(config) {
+  images: {
+    domains: ['courses-top.ru']
+  },
+  webpack(config, options) {
     config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+        titleProp: true,
+      },
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
     });
 
     return config;
-  }
+  },
 };
