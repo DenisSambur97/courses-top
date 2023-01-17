0935422caa6bcd09f7671e04ba4ac108be372e07
Denis <denissand1997@gmail.com>
2023-01-17 16:56:45 +0300

    First version menu

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,47 +1,78 @@
-import styles from './Menu.module.css'
 import {useContext} from "react";
+import styles from './Menu.module.css'
 import {AppContext} from "../../context/app.context";
-import {FirstLevelMenuItem} from "../../interfaces/menu.interface";
-import Courses from './icons/Courses.svg'
+import {FirstLevelMenuItem, PageItem} from "../../interfaces/menu.interface";
+import CoursesIcon from "./icons/Courses.svg"
+import ServicesIcon from "./icons/Services.svg"
+import BooksIcon from "./icons/Books.svg"
+import ProductsIcon from "./icons/Products.svg"
+import {TopLevelCategory} from "../../interfaces/page.interface";
+import cn from "classnames";
 
-// const CoursesIcon = <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path
-//         d="M24 15.4055C24 14.4901 23.4104 13.7169 22.5938 13.4257V6.14159L24 5.56179L12 0.74707L0 5.56179L12 10.3766L21.1875 6.70624V13.4257C20.3708 13.7169 19.7812 14.4901 19.7812 15.4055C19.7812 16.2437 20.2762 16.9631 20.986 17.3031L19.8177 20.8081L21.1511 21.253L21.8906 19.0345L22.6301 21.253L23.9636 20.8081L22.7953 17.3031C23.505 16.9632 24 16.2437 24 15.4055Z"
-//         fill="#7653FC"/>
-// </svg>
-//
-// const CoursesIcon = <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path
-//         d="M24 15.4055C24 14.4901 23.4104 13.7169 22.5938 13.4257V6.14159L24 5.56179L12 0.74707L0 5.56179L12 10.3766L21.1875 6.70624V13.4257C20.3708 13.7169 19.7812 14.4901 19.7812 15.4055C19.7812 16.2437 20.2762 16.9631 20.986 17.3031L19.8177 20.8081L21.1511 21.253L21.8906 19.0345L22.6301 21.253L23.9636 20.8081L22.7953 17.3031C23.505 16.9632 24 16.2437 24 15.4055Z"
-//         fill="#7653FC"/>
-// </svg>
-//
-// const CoursesIcon = <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path
-//         d="M24 15.4055C24 14.4901 23.4104 13.7169 22.5938 13.4257V6.14159L24 5.56179L12 0.74707L0 5.56179L12 10.3766L21.1875 6.70624V13.4257C20.3708 13.7169 19.7812 14.4901 19.7812 15.4055C19.7812 16.2437 20.2762 16.9631 20.986 17.3031L19.8177 20.8081L21.1511 21.253L21.8906 19.0345L22.6301 21.253L23.9636 20.8081L22.7953 17.3031C23.505 16.9632 24 16.2437 24 15.4055Z"
-//         fill="#7653FC"/>
-// </svg>
-//
-// const CoursesIcon = <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path
-//         d="M24 15.4055C24 14.4901 23.4104 13.7169 22.5938 13.4257V6.14159L24 5.56179L12 0.74707L0 5.56179L12 10.3766L21.1875 6.70624V13.4257C20.3708 13.7169 19.7812 14.4901 19.7812 15.4055C19.7812 16.2437 20.2762 16.9631 20.986 17.3031L19.8177 20.8081L21.1511 21.253L21.8906 19.0345L22.6301 21.253L23.9636 20.8081L22.7953 17.3031C23.505 16.9632 24 16.2437 24 15.4055Z"
-//         fill="#7653FC"/>
-// </svg>
-
-// const firstLevelMenu: FirstLevelMenuItem = [
-//     {route: 'courses', name: 'Курсы', icon: {Courses}}
-// ]
-
+const firstLevelMenu: FirstLevelMenuItem[] = [
+    {route: 'courses', name: 'Курсы', icon: <CoursesIcon/>, id: TopLevelCategory.Courses},
+    {route: 'services', name: 'Сервисы', icon: <ServicesIcon/>, id: TopLevelCategory.Services},
+    {route: 'books', name: 'Книги', icon: <BooksIcon/>, id: TopLevelCategory.Books},
+    {route: 'products', name: 'Продукты', icon: <ProductsIcon/>, id: TopLevelCategory.Products}
+]
 
 export const Menu = (): JSX.Element => {
     const {menu, setMenu, firstCategory} = useContext(AppContext)
 
+    // Функция которая создает пункты меню с верхнего уровня
+    const buildFirstLevelMenu = () => {
+        return(
+            <>
+                {firstLevelMenu.map(menu => (
+                    <div key={menu.route}>
+                        <a href={`/${menu.route}`}>
+                            <div className={cn(styles.firstLevel, {
+                                [styles.firstLevelActive] : menu.id == firstCategory
+                            })}>
+                                {menu.icon}
+                                <span>{menu.name}</span>
+                            </div>
+                        </a>
+                        {/* Если определенный пунт открыт, тогда открывается его подпункты*/}
+                        {menu.id == firstCategory && buildSecondLevelMenu(menu)}
+                    </div>
+                ))}
+            </>
+        )
+    }
+
+    const buildSecondLevelMenu = (menuItem: FirstLevelMenuItem) => {
+        return(
+            <div className={styles.secondBlock}>
+                {menu.map(m => (
+                    <div key={m._id.secondCategory}>
+                        <div className={styles.secondLevel}>{m._id.secondCategory}</div>
+                        <div className={cn(styles.secondLevelBlock, {
+                            [styles.secondLevelBlockOpened]: m.isOpened
+                        })}>
+                            {buildThirdLevelMenu(m.pages, menuItem.route)}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+    const buildThirdLevelMenu = (pages: PageItem[], route: string) => {
+        return (
+            pages.map(p => (
+                <a key={p._id} href={`/${route}/${p.alias}`} className={cn(styles.thirdLevel, {
+                    [styles.thirdLevelActive]: false
+                })}>
+                    {p.category}
+                </a>
+            ))
+        )
+    }
+
     return (
-        <div>
-            {Courses}
-            <ul>
-                {menu.map(m => <li key={m._id.secondCategory}>{m._id.secondCategory}</li>)}
-            </ul>
+        <div className={styles.menu}>
+            {buildFirstLevelMenu()}
         </div>
     )
 }
