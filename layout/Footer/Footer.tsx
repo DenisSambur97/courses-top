00e895a4717bb685efa5a2cc10b2c03fa98c2a27
Denis <denissand1997@gmail.com>
2023-01-18 19:21:34 +0300

    Modify New Menu v2

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import {Parag} from "../../components";
 export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
         <footer className={cn(className, styles.footer)} {...props}>
-            <Parag className={cn(className,styles.roots)}>OwlTop © {new Date().getUTCFullYear()} Все права защищены</Parag>
+            <Parag className={cn(className,styles.roots)}>OwlTop ©{new Date().getUTCFullYear()} Все права защищены</Parag>
             <a href='#' target='_blank' className={cn(className,styles.agrees)}>Пользовательское соглашение</a>
             <a href='#' target='_blank' className={cn(className,styles.conf)}>Политика конфиденциальности</a>
         </footer>
---
+++ pages/[type]/[alias].tsx
@@ -0,0 +1,91 @@
+import {withLayout} from "../../layout/Layout";
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from "next";
+import axios from "axios";
+import {MenuItem} from "../../interfaces/menu.interface";
+import {TopLevelCategory, TopPageModal} from "../../interfaces/page.interface";
+import {ParsedUrlQuery} from "querystring";
+import {notFound} from "next/navigation";
+import {ProductModel} from "../../interfaces/product.interface";
+import {firstLevelMenu} from "../../helpers/helpers";
+import {type} from "os";
+import {router} from "next/client";
+
+function TopPage({menu, page, products}: TopPageProps): JSX.Element {
+    return (
+        <>
+            {products && products.length}
+        </>
+    )
+}
+
+export default withLayout(TopPage)
+
+// Какие пути для постраения страницы
+export const getStaticPaths: GetStaticPaths = async () => {
+    let paths: string[] = []
+    for (const m of firstLevelMenu){
+        //Меню первого уровня
+        const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+            firstCategory: m.id
+        })
+        paths = paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`))) // разложили в плоский массив URL'ов (чтобы не было массива в массиве))
+    }
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+// Получение статически для кэширования
+export const getStaticProps: GetStaticProps<TopPageProps> = async ({params}: GetStaticPropsContext<ParsedUrlQuery>) => {
+    if(!params){
+        return {
+            notFound: true
+        }
+    }
+    const firstCategoryItem = firstLevelMenu.find(m => m.route == params.type)
+    if(!firstCategoryItem){
+        return {
+            notFound: true
+        }
+    }
+    try {
+        //Меню первого уровня
+        const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+            firstCategory: firstCategoryItem.id
+        })
+        if(menu.length == 0){
+            return {
+                notFound: true
+            }
+        }
+        //Аллиас для основного контента
+        const {data: page} = await axios.get<TopPageModal>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/byAlias/' + params.alias)
+        //Получение продуктов для основного контента
+        const {data: products} = await axios.post<ProductModel[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find', {
+            category: page.category,
+            limit: 10
+        })
+
+        return {
+            props: {
+                menu,
+                firstCategory: firstCategoryItem.id,
+                page,
+                products
+            }
+        }
+    }catch{
+        return {
+            notFound: true
+        }
+    }
+}
+
+interface TopPageProps extends Record<string, unknown>{
+    menu: MenuItem[]
+    firstCategory: TopLevelCategory
+    page: TopPageModal
+    products: ProductModel[]
+}
+
